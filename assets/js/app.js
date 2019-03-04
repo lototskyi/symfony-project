fae2b7b8c8a987d4ef0c4331efaf04da06a1203d
alex <o.lototskyi@gmail.com>
2019-03-04 16:34:05 +0200

    day 14: translations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,11 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+// require jQuery normally
+var $ = require('jquery');
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// var $ = require('jquery');
+require('bootstrap');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
